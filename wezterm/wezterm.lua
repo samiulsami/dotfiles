5042b9d7878846ae63bb4960c11c01e92acebc87
sami <samiulsami7786@gmail.com>
2026-02-18 11:37:43 +0600

    Port ghostty colors into wezterm
    Signed-off-by: sami <samiulsami7786@gmail.com>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,33 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.colors = {
-	background = "#000000",
 	foreground = "#CECECE",
+	background = "#000000",
+	cursor_bg = "#CECECE",
+	cursor_fg = "#000000",
+	cursor_border = "#CECECE",
+	selection_fg = "#000000",
+	selection_bg = "#CECECE",
+	ansi = {
+		"#000000", -- black
+		"#cc6666", -- red
+		"#b5bd68", -- green
+		"#f0c674", -- yellow
+		"#81a2be", -- blue
+		"#b294bb", -- magenta
+		"#8abeb7", -- cyan
+		"#c5c8c6", -- white
+	},
+	brights = {
+		"#4a4a4a", -- bright black
+		"#d54e53", -- bright red
+		"#b9ca4a", -- bright green
+		"#e7c547", -- bright yellow
+		"#7aa6da", -- bright blue
+		"#c397d8", -- bright magenta
+		"#70c0b1", -- bright cyan
+		"#eaeaea", -- bright white
+	},
 }
 
 config.window_padding = {
